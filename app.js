93f2467b91aeaa96d10f3387bc272fcd740f57cc
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-12-01 19:46:14 +0100

    exo 66 fini

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 
 
-let entier = document.getElementById("entier");
-let flottant = document.getElementById("flottant");
-let nonNum = document.getElementById("non-numerique");
-let rand = document.getElementById("random");
-let trunc = document.getElementById("trunc");
-let repl = document.getElementById("replace");
+let p1 = document.getElementById("entier");
+let p2 = document.getElementById("flottant");
+let par = document.getElementById("non-numerique");
+let p4 = document.getElementById("random");
+let p5 = document.getElementById("trunc");
+let p6 = document.getElementById("replace");
 
 let operande = 10;
 
-entier.innerHTML = (32 * operande).toString()
+ p1.innerHTML = (p1.innerHTML * operande);
 
-flottant.innerHTML = (54.85 * operande) + entier.toString()
+p2.innerHTML = ((parseFloat(p2.innerHTML) * operande) + parseFloat(p1.innerHTML));
+
+
+if (isNaN(par)){
+    par.innerHTML = "0";
+}
+
+let aleatoire;
+for (let i = 0; i < 2; i+=1){
+    aleatoire = Math.trunc(Math.random() * 100);
+    p4.innerHTML = aleatoire;
+}
+
+p5.innerHTML = Math.trunc(p5.innerHTML);
+
+
+p6.innerHTML = p6.innerHTML.replace("world","Mika.B");
+
+
+
+
+
+
+
+
